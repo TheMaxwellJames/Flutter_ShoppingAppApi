24253629c6b05e14f6d6ee809f880d151a8d5fc3
Maxwell James <maxichanj@gmail.com>
2023-08-11 23:01:07 +0100

    generated jwt token

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,26 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const User = require("../model/user.model");
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 router.route("/").post(async (req, res) => {
     console.log("Login hit", req.body);
-    
+
     try {
         const result = await User.findOne({ mobileNo: req.body.mobileNo });
-        
+
         if (result !== null) {
             // login
+            sendToken(req.body.mobileNo, "Login Successful", res);
             console.log("Login");
         } else {
             // register
+            const user = new User({
+                mobileNo: req.body.mobileNo
+            });
+            user.save().then(() => {
+                sendToken(req.body.mobileNo, "Register Successful", res);
+            }).catch((err) => { 
+             res.json({ token: err, msg: "Error" });
+             });
             console.log("Register");
         }
 
-        return res.json({ msg: "Great, welcome" });
+       
     } catch (error) {
         return res.status(403).json({ msg: "An error occurred" });
     }
 });
+
+const sendToken = (mobileNo, msg, res) => {
+    let token = jwt.sign({ mobileNo: mobileNo },
+        "this is shopping app"
+        );
+res.json({ token: token, msg: msg });
+}
 
 module.exports = router;
 
